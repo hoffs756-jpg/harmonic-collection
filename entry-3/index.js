4a3d5bd1a2372aa8e4f74b009c921616594b1ca8
hoffs756-jpg <hoffs756@newschool.edu>
2025-12-06 18:56:39 +0000

    annoying

---
+++ entry-3/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".cute-btn");
+    setTimeout(() => {
+        button.style.transition = "opacity 0.8s ease";
+        button.style.opacity = "1";
+    }, 300);
+});
