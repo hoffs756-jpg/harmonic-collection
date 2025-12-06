c60f204526d6c003652c383f4585a8a83ed58696
hoffs756-jpg <hoffs756@newschool.edu>
2025-12-06 19:26:22 +0000

    limeey

---
+++ entry-4/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Fade-in button
+    const button = document.querySelector('.cute-btn');
+    if (button) {
+        setTimeout(() => {
+            button.style.opacity = '1';
+        }, 500);
+    }
+
+    // Animate image
+    const image = document.querySelector('.recipe-img');
+    if (image) {
+        setTimeout(() => {
+            image.style.opacity = '1';
+            image.style.transform = 'scale(1)';
+        }, 400);
+    }
+
+    // Animate ingredient and instruction lists
+    const lists = document.querySelectorAll('.list');
+    lists.forEach((list, i) => {
+        setTimeout(() => {
+            list.style.opacity = '1';
+            list.style.transform = 'scale(1)';
+        }, 300 + i * 150);
+    });
+});
